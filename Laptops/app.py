cc727d6c60e36a43e3b055d94a23039d9b782786
billy <GalaxCherryL@gmail.com>
2022-07-09 23:35:47 +0200

    refactor

--- Laptops/app.py
+++ Laptops/app.py
@@ -3,42 +3,46 @@ import pickle
 import numpy as np
 import pandas as pd
 
-df=pd.read_csv('df.csv')
+df = pd.read_csv('df.csv')
 
 pipe = pickle.load(open("pipe.pkl", "rb"))
 
 st.title('Laptop Price Predictor')
 
-#Brand
-company = st.selectbox('Brand',df['Company'].unique())
+# Brand
+company = st.selectbox('Brand', df['Company'].unique())
 
-#Type of Laptop
+# Type of Laptop
 laptop_type = st.selectbox("Type", df['TypeName'].unique())
 
-#Ram
+# Ram
 ram = st.selectbox("Ram", df['Ram'].sort_values(ascending=False).unique())
 
-#Weight
+# Weight
 weight = st.selectbox("Weight", df['Weight'].sort_values(ascending=False).unique())
 
-#TouchScreen
+# TouchScreen
 touchscreen = st.selectbox("TouchScreen", ['No', 'Yes'])
 
-#IPS
+# IPS
 ips = st.selectbox("IPS", ['No', 'Yes'])
 
-#Screen Size
-screen_size = st.selectbox('Screen Size (in Inches)', [10.1, 11.3, 11.6, 12. , 12.3, 12.5, 13. , 13.3, 13.5, 13.9, 14. ,14.1, 15. , 15.4, 15.6, 17. , 17.3, 18.4])
+# Screen Size
+screen_size = st.selectbox('Screen Size (in Inches)',
+                           [10.1, 11.3, 11.6, 12., 12.3, 12.5, 13., 13.3, 13.5, 13.9, 14., 14.1, 15., 15.4, 15.6, 17.,
+                            17.3, 18.4])
 
-#Resolution
-resolution = st.selectbox('Screen Resolution', ['1366x768','1600x900','1920x1080','2304x1440','2560x1440','2560x1600','2880x1800','3200x1800','3840x2160'])
+# Resolution
+resolution = st.selectbox('Screen Resolution',
+                          ['1366x768', '1600x900', '1920x1080', '2304x1440', '2560x1440', '2560x1600', '2880x1800',
+                           '3200x1800', '3840x2160'])
 
-#Specs
-cpu= st.selectbox('CPU',df['Cpu_brand'].unique())
-hdd= st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
-ssd= st.selectbox('SSD(in GB)',[0,128,256,512,1024])
-gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
-os = st.selectbox('OS',df['Os'].unique())
+# Specs
+cpu = st.selectbox('CPU', df['Cpu_brand'].unique())
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
+ssd = st.selectbox('SSD(in GB)', [0, 128, 256, 512, 1024])
+gpu = st.selectbox('GPU', df['Gpu_brand'].unique())
+os = st.selectbox('OS', df['Os'].unique())
 
 # Prediction
 
@@ -52,12 +56,12 @@ if st.button('Predict Price'):
         ips = 1
     else:
         ips = 0
-    
+
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res **2) + (Y_res **2)) / screen_size
+    ppi = ((X_res ** 2) + (Y_res ** 2)) / screen_size
 
-    query = np.array([company,laptop_type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
-    query = query.reshape(1,12)
+    query = np.array([company, laptop_type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    query = query.reshape(1, 12)
     prediction = str(int(np.exp(pipe.predict(query)[0])))
-    st.title("The predicted price of the configuartion is " + prediction)
+    st.title("The predicted price of the configuration is " + prediction)
